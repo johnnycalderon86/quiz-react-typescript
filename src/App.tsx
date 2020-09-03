9a1d601ef2dfd09fbd048b151ee6c973a56c835d
Johnny <johnnycalderondeveloper@gmail.com>
2020-09-03 21:15:14 +0200

    changing ts config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 
 
+
 function App() {
+
+  const startTrivia = async () => {}
+
+const checkAnswer = (e : React.MouseEvent<HTMLButtonElement>)=>{
+
+}
+
+const nextQuestion = ()=>{
+
+}
+
+
+
   return (
     <div className="App">
      <h1>Quiz</h1>
